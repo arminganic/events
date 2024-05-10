6d60977e92fd9de01f13b2c18742af3e327bdcf2
Armin Ganic <armin.ganic@gmail.com>
2024-05-10 20:58:24 +0200

    Added contacts to EventPage

--- src/app/event-page/event-page.component.ts
+++ src/app/event-page/event-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { EventService } from '../../shared/services/event.service';
 
 @Component({
   selector: 'app-event-page',
@@ -7,4 +10,18 @@ import { Component } from '@angular/core';
   templateUrl: './event-page.component.html',
   styleUrl: './event-page.component.css',
 })
-export class EventPageComponent {}
+export class EventPageComponent {
+  private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly eventService = inject(EventService);
+
+  private readonly paramMap = toSignal(this.activatedRoute.paramMap);
+
+  private readonly id = computed(() => this.paramMap()?.get('id') ?? undefined);
+  protected readonly event = computed(() => {
+    const id = this.id();
+    if (!id) {
+      return undefined;
+    }
+    return this.eventService.getEventById(id)();
+  });
+}
